75a1a2652cd4e6bb03aaaff32b149c3d06eb30f6
coder-abdo <a.h2020.2007@gmail.com>
2023-06-12 02:25:11 +0300

    remove folders

--- src/Types/Types.ts
+++
@@ -1,6 +0,0 @@
-type Cell = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
-type Field = Cell[][];
-type Coords = [number, number];
-type CellStateType = Record<string, Cell>;
-
-export { Cell, Field, Coords, CellStateType };
